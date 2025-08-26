15f66e3bdc17ec1ad0be54b48b6e7b3d92f95616
IamFaiz2024 <ImFaizA2024@hotmail.com>
2025-08-26 14:22:44 +0400

    TransferToUSB method working
    TransferToUSB method working but need to polish more

--- MaintenanceExtApp/Controllers/ToolsController.cs
+++ MaintenanceExtApp/Controllers/ToolsController.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using MaintenanceExtApp.Data;
+﻿using MaintenanceExtApp.Data;
 using MaintenanceExtApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Renci.SshNet;
+using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
-using Renci.SshNet;
 
 namespace MaintenanceExtApp.Controllers
 {
@@ -32,34 +34,49 @@ namespace MaintenanceExtApp.Controllers
         [HttpPost]
         public IActionResult TransferToUSB(int windowsHostId, int linuxHostId)
         {
-            // Your transfer logic here
-            //Console.WriteLine($"Transfer2USB Button pressed - Windows ID: {windowsHostId}, Linux ID: {linuxHostId}");
-            //return RedirectToAction("Transfer2USB");
             try
             {
+                Console.WriteLine($"Transfer started - Windows ID: {windowsHostId}, Linux ID: {linuxHostId}");
+
                 var windowsHost = _context.Hosts.Find(windowsHostId);
                 var linuxHost = _context.Hosts.Find(linuxHostId);
 
-                var zipFileName = DateTime.Now.ToString("yyyyMMddhhmm") + ".zip";
+                Console.WriteLine($"Windows Host: {windowsHost.HostName}, Linux Host: {linuxHost.HostName}");
+
+                var zipFileName = DateTime.Now.ToString("yyMMMddHHmm", CultureInfo.InvariantCulture).ToUpper() + ".zip";
                 var tempZipPath = Path.Combine(Path.GetTempPath(), zipFileName);
 
-                ZipFilesFromSSHHost(windowsHost, tempZipPath);
+                Console.WriteLine($"Creating zip: {tempZipPath}");
 
-                string remotePath = ""; // Declare outside
+                ZipFilesUsingPowerShell(windowsHost, tempZipPath);
+                Console.WriteLine("Zip created successfully");
 
+                string remotePath = "";
                 using (var scp = new ScpClient(linuxHost.HostAddress, linuxHost.HostUserName, linuxHost.HostPassword))
                 {
+                    Console.WriteLine("Connecting to Linux host...");
                     scp.Connect();
+                    Console.WriteLine("Connected to Linux host");
+
                     remotePath = $"{linuxHost.HostSharedPath}/{zipFileName}";
+                    Console.WriteLine($"Uploading to: {remotePath}");
+
                     scp.Upload(new FileInfo(tempZipPath), remotePath);
+                    Console.WriteLine("Upload completed");
+
                     scp.Disconnect();
                 }
 
                 System.IO.File.Delete(tempZipPath);
+                Console.WriteLine("Temp file cleaned up");
+
                 TempData["Message"] = $"Transfer completed! File: {remotePath}";
+                Console.WriteLine("Transfer successful");
             }
             catch (Exception ex)
             {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                Console.WriteLine($"Stack Trace: {ex.StackTrace}");
                 TempData["Error"] = $"Transfer failed: {ex.Message}";
             }
 
@@ -76,18 +93,19 @@ namespace MaintenanceExtApp.Controllers
             return RedirectToAction("Transfer2USB");
         }
 
-        private void ZipFilesFromSSHHost(Models.Host host, string zipOutputPath)
+        private void ZipFilesUsingPowerShell(Models.Host windowsHost, string zipOutputPath)
         {
-            using (var sftp = new SftpClient(host.HostAddress, host.HostUserName, host.HostPassword))
+            var psi = new ProcessStartInfo
             {
-                sftp.Connect();
+                FileName = "powershell.exe",
+                Arguments = $"-Command \"Compress-Archive -Path '\\\\{windowsHost.HostAddress}\\{windowsHost.HostSharedPath}\\*' -DestinationPath '{zipOutputPath}'\"",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
 
-                using (var zip = ZipFile.Open(zipOutputPath, ZipArchiveMode.Create))
-                {
-                    DownloadDirectoryToZip(sftp, host.HostSharedPath, "", zip);
-                }
-
-                sftp.Disconnect();
+            using (var process = Process.Start(psi))
+            {
+                process.WaitForExit();
             }
         }
 
