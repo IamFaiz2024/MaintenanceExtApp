1d99011d982e01d3143a79bcbd78e4a83ea6ef70
IamFaiz2024 <ImFaizA2024@hotmail.com>
2025-08-28 23:34:16 +0400

    DataTransfer

---
+++ MaintenanceExtApp/Controllers/DataTransferController.cs
@@ -0,0 +1,26 @@
+﻿using MaintenanceExtApp.Data;
+using MaintenanceExtApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Renci.SshNet;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+
+namespace MaintenanceExtApp.Controllers
+{
+    public class DataTransferController : Controller
+    {
+        private readonly MaintenanceDbContext _context;
+        public DataTransferController(MaintenanceDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
